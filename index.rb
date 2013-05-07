4c793b64d3766be76517dec1c8962d266fd642e7
mgpyone <mgpyone@mysteryzillion.com>
2013-05-07 16:20:13 +0630

    Connected to db

--- index.rb
+++ index.rb
@@ -1,1 +1,18 @@
 require 'sinatra'
+require 'sqlite3'
+
+get '/' do
+  begin
+
+    db = SQLite3::Database.open 'test.db'
+    "Success!"
+
+  rescue SQLite3::Exception => e
+
+    puts "Exception Occured"
+    puts e
+
+  ensure
+    db.close if db
+  end
+end
